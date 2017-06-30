4bbfe9893fd3efc4e7d01bc474f532ae0afe4804
Joriwal <himalayj@amazon.com>
2017-06-30 08:06:01 -0700

    Converting parrallel calls to sequential using Promise-seq

--- exoplanet-explorer/app/scripts/app.js
+++ exoplanet-explorer/app/scripts/app.js
@@ -1,16 +1,16 @@
 /*
-Instructions:
-(1) Get the planet data and add the search header.
-(2) Create the first thumbnail with createPlanetThumb(data)
-(3) Handle errors!
-  (a) Pass 'unknown' to the search header.
-  (b) console.log the error.
+ Instructions:
+ (1) Refactor .forEach below to create a sequence of Promises that always resolves in the same
+ order it was created.
+ (a) Fetch each planet's JSON from the array of URLs in the search results.
+ (b) Call createPlanetThumb on each planet's response data to add it to the page.
+ (2) Use developer tools to determine if the planets are being fetched in series or in parallel.
  */
 
 // Inline configuration for jshint below. Prevents `gulp jshint` from failing with quiz starter code.
 /* jshint unused: false */
 
-(function(document) {
+(function (document) {
   'use strict';
 
   var home = null;
@@ -36,7 +36,7 @@ Instructions:
   }
 
   /**
-   * XHR wrapped in a promise
+   * XHR wrapped in a promise.
    * @param  {String} url - The URL to fetch.
    * @return {Promise}    - A Promise that resolves when the XHR succeeds and fails otherwise.
    */
@@ -52,30 +52,33 @@ Instructions:
    * @return {Promise}    - A promise that passes the parsed JSON response.
    */
   function getJSON(url) {
-    return get(url).then(function(response) {
+    return get(url).then(function (response) {
       return response.json();
     });
   }
 
-  window.addEventListener('WebComponentsReady', function() {
+  window.addEventListener('WebComponentsReady', function () {
     home = document.querySelector('section[data-route="home"]');
     /*
-    Uncomment the next line and start here when you're ready to add the first thumbnail!
-
-    Your code goes here!
+     Refactor this code!
      */
-                          getJSON('../data/earth-like-results.json')
-                          .then(function(response) {
-                                console.log(response);
-                                addSearchHeader(response.query);
-                                response.results.forEach(function(resp) {
-                                        getJSON(resp).then(createPlanetThumb)
-                                })
-                           })
-                          .then(createPlanetThumb)
-                          .catch(function(error) {
-                                 console.log(error);
-                                 addSearchHeader('unknown');
-                                 })
+    getJSON('../data/earth-like-results.json')
+      .then(function (response) {
+
+        console.log(response);
+        addSearchHeader(response.query);
+
+        var seq = Promise.resolve();
+
+        response.results.forEach(function (url) {
+          seq = seq.then(function () {
+            return getJSON(url);
+          }).then(createPlanetThumb);
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+        addSearchHeader('unknown');
+      });
   });
 })(document);
